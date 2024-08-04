65c126c3078c1fb45b6762213cc2964a28fe31e8
Christopher Adrigan <christopher.adrigan@gmail.com>
2024-08-04 18:04:55 +0200

    added private subnets

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -20,9 +20,9 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-####################################################
-# Public subnet a  for load balancer public access #
-####################################################
+###################################################
+# Public subnet a for load balancer public access #
+###################################################
 
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
@@ -54,9 +54,9 @@ resource "aws_route" "public_internet_access_a" {
   gateway_id             = aws_internet_gateway.main.id
 }
 
-####################################################
-# Public subnet b  for load balancer public access #
-####################################################
+###################################################
+# Public subnet b for load balancer public access #
+###################################################
 
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
@@ -86,4 +86,30 @@ resource "aws_route" "public_internet_access_b" {
   route_table_id         = aws_route_table.public_b.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
+}
+
+#############################################
+# Private subnet a for internal access only #
+#############################################
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.10.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+  tags = {
+    Name = "${local.prefix}-private-a"
+  }
+}
+
+#############################################
+# Private subnet b for internal access only #
+#############################################
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.11.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+  tags = {
+    Name = "${local.prefix}-private-b"
+  }
 }
